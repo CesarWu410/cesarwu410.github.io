f12c2c7c6483d3af4adb9823616760840313150d
CesarWu <cesarwu@foxmail.com>
2026-08-11 17:09:09 +0800

    copy _site

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,24 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1786438870',resources: [
+      '/cesarwu410.github.io/assets/css/jekyll-theme-chirpy.css',
+      '/cesarwu410.github.io/',
+      
+        '/cesarwu410.github.io/categories/',
+      
+        '/cesarwu410.github.io/tags/',
+      
+        '/cesarwu410.github.io/archives/',
+      
+        '/cesarwu410.github.io/about/',
+      
+
+      
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
